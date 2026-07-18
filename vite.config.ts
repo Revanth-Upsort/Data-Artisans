1fd56a8d525f14540eb49bddf6ad43cd66c21223
Revanth-Upsort <upsortcareers.da02@gmail.com>
2026-07-18 14:55:34 +0530

    change 6

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,6 @@ import path from 'node:path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  // Served from a GitHub Pages project subpath:
-  // https://kranthisai07.github.io/Website-Data-Artisans/
   base: '/Website-Data-Artisans/',
   plugins: [react()],
   resolve: {
